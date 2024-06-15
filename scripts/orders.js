9f0b1f6eb8c04b50a8819f8ec59b4f90d7c55e52
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2024-06-15 09:47:20 -0300

    Atualização

---
+++ scripts/orders.js
@@ -0,0 +1,58 @@
+const Orders = [
+{
+    productName: 'Placa de Video RTX 3060',
+    productNumber: '64857',
+    paymentStatus: 'Devido',
+    shipping: 'Pendente'
+},
+
+{
+    productName: 'Fonte MSI MAG A650BN',
+    productNumber: '82915',
+    paymentStatus: 'Cancelado',
+    shipping: 'Cancelado'
+},
+
+{
+    productName: 'Memória RAM Kingston Fury Beast, 8GB',
+    productNumber: '67195',
+    paymentStatus: 'Pago',
+    shipping: 'Pendente'
+},
+
+{
+    productName: 'SSD SATA Kingston, A400, 240GB',
+    productNumber: '97315',
+    paymentStatus: 'Pago',
+    shipping: 'Enviado'
+},
+
+{
+    productName: 'Water Cooler Rise Mode, ARGB',
+    productNumber: '64857',
+    paymentStatus: 'Pago',
+    shipping: 'Enviado'
+},
+
+{
+    productName: 'Memória RAM Rise Mode Z, 8GB',
+    productNumber: '41258',
+    paymentStatus: 'Pago',
+    shipping: 'Pendente'
+},
+
+{
+    productName: 'Placa Mãe Gigabyte B550M',
+    productNumber: '36795',
+    paymentStatus: 'Pago',
+    shipping: 'Entregue'
+},
+
+,{
+    productName: 'Processador AMD Ryzen 5 4600G',
+    productNumber: '16748',
+    paymentStatus: 'Cancelado',
+    shipping: 'Cancelado'
+}
+
+]
